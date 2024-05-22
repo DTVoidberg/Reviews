a11c0f79572a2a440da526952a19c8975dfe5e1f
Deividas Terechovas <davidmabbitt1994@gmail.com>
2024-05-22 18:55:28 +0200

    Initial Commit

---
+++ TextBasedRPG/TextBasedRPG/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Numerics;
+using System;
+using System.Net.Http.Headers;
+using System.Xml;
+
+namespace TextBasedRPG;
+
+
+public class RPG
+{
+
+    public static void Main(string[] args)
+    {
+        Unit player = new Unit(100, 20, 15, "Player");
+        Unit enemy1 = new Unit(75, 10, 12, "Dark Mage");
+        Unit enemy2 = new Unit(150, 12, 5, "Orc");
+        Random random = new Random();
+
+        int rand = random.Next(0, 2);
+
+        Console.WriteLine("Welcome to this Text Based RPG!\nLet's Start!");
+
+        {
+
+
+            while (!player.IsDead && !enemy1.IsDead)
+            {
+
+                Console.WriteLine($"{player.UnitName} HP = {player.HP}.\n{enemy1.UnitName} HP = {enemy1.HP}");
+                Console.WriteLine("Player's turn, What will you do?\nPress A to Attack or Press D to Heal");
+                string choice = Console.ReadLine();
+
+                //var result = choice == "a" ? player.Attack(enemy1) : choice == "d" ? player.Heal : player.UserInput;
+                if (choice == "a")
+                {
+                    player.Attack(enemy1);
+                }
+                else if (choice == "d")
+                {
+                    player.Heal();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input!.");
+                }
+                if (player.IsDead && !enemy1.IsDead)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{player.UnitName} HP = {player.HP}. {enemy1.UnitName} HP = {enemy1.HP}");
+
+                Console.WriteLine("Enemies Turn!");
+
+                if (rand == 0)
+                {
+                    enemy1.Attack(player);
+                }
+                else
+                {
+                    enemy1.Heal();
+                }
+                //Hello
+
+                while (!player.IsDead && !enemy2.IsDead && enemy1.IsDead)
+                {
+                    Console.WriteLine($"{player.UnitName} HP = {player.HP}.\n{enemy2.UnitName} HP = {enemy2.HP}");
+                    Console.WriteLine("Player's turn, What will you do?\nPress A to Attack or Press D to Heal");
+                    string choice2 = Console.ReadLine();
+
+                    if (choice2 == "a")
+                    {
+                        player.Attack(enemy2);
+                    }
+                    else if (choice2 == "d")
+                    {
+                        player.Heal();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid input!.");
+                    }
+                    if (player.IsDead && !enemy2.IsDead)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine($"{player.UnitName} HP = {player.HP}. {enemy2.UnitName} HP = {enemy2.HP}");
+
+                    Console.WriteLine("Enemies Turn!");
+
+                    if (rand == 0)
+                    {
+                        enemy2.Attack(player);
+                    }
+                    else
+                    {
+                        enemy2.Heal();
+                    }
+
+                    if (!player.IsDead && enemy2.IsDead)
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ TextBasedRPG/TextBasedRPG/Unit.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal class Unit
+    {
+        private int unitMaxHP;
+        private int unitCurrentHP;
+        private int unitCurrentMana;
+        private int unitMaxMana;
+        private int healPower;
+        private int attackPower;
+        private int userStrengh;
+        private int userDexterity;
+        private int userIntelligence;
+        private int userArmor;
+        private string unitName;
+        private Random random;
+
+        public int HP { get { return unitCurrentHP; } }
+        public string UnitName { get { return unitName; } }
+        public bool IsDead { get { return unitCurrentHP <= 0;} }
+
+
+
+        public Unit(int unitMaxHP, int attackPower, int healPower, string unitName)
+        {
+            this.unitMaxHP = unitMaxHP;
+            this.unitCurrentHP = unitMaxHP;
+            this.attackPower = attackPower;
+            this.healPower = healPower;
+            this.unitName = unitName;
+            this.random = new Random();
+        }
+        public void Attack(Unit unitToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randomDamage = (int)(attackPower * rng);
+            unitToAttack.TakeDamage(randomDamage);
+            Console.WriteLine($"{unitName} attack {unitToAttack.unitName} and deals {randomDamage} damage!");
+        }
+        public void TakeDamage(int damage)
+        {
+            unitCurrentHP -= damage;
+
+            if (IsDead)
+            {
+                Console.WriteLine($"{UnitName} has been slain!.");
+                
+            }
+
+        }
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            unitCurrentHP = heal + unitCurrentHP > unitMaxHP ? unitMaxHP : unitCurrentHP + heal;
+            Console.WriteLine($"{UnitName}heals {heal}HP");
+        }
+        public void Armor()
+        {
+
+        }
+    }
+}
